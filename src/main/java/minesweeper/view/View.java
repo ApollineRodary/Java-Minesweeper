66349145dc992a2c6a23aba48912f65569011919
Apolline Rodary <119676876+ApollineRodary@users.noreply.github.com>
2023-01-16 22:52:20 +0100

    Add message dialogs for victory and defeat

--- src/main/java/minesweeper/view/View.java
+++ src/main/java/minesweeper/view/View.java
@@ -4,6 +4,8 @@ import java.beans.IndexedPropertyChangeEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import javax.swing.JOptionPane;
+
 import minesweeper.model.Cell;
 import minesweeper.model.GameState;
 
@@ -26,14 +28,14 @@ public class View implements PropertyChangeListener {
                 frame.getPanel().repaint();
         
                 switch ((Cell) e.getNewValue()) {
-                    case MINE:
-                        SoundMixer.playSound(SoundEvent.REVEALED_MINE);
-                        break;
                     case FLAGGED:
                         SoundMixer.playSound(SoundEvent.PLACED_FLAG);
                         break;
                     case HIDDEN:
                         SoundMixer.playSound(SoundEvent.REMOVED_FLAG);
+                        break;
+                    case MINE:
+                    case FALSE_FLAG:
                         break;
                     default:
                         SoundMixer.playSound(SoundEvent.REVEALED_CELL);
@@ -46,8 +48,20 @@ public class View implements PropertyChangeListener {
                 break;
             
             case "gameState":
-                frame.getRestartButton().updateIcon((GameState) e.getNewValue());
+                GameState state = (GameState) e.getNewValue();
+                frame.getRestartButton().updateIcon(state);
+                if (state==GameState.DEFEAT) announceDefeat();
+                else if (state==GameState.VICTORY) announceVictory();
                 break;
         }
+    }
+
+    private void announceDefeat() {
+        SoundMixer.playSound(SoundEvent.REVEALED_MINE);
+        JOptionPane.showMessageDialog(frame, "You lose!", "Defeat", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    private void announceVictory() {
+        JOptionPane.showMessageDialog(frame, "You win!", "Victory", JOptionPane.PLAIN_MESSAGE);
     }
 }
